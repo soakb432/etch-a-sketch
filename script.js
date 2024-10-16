89205c9bbae00048592c5a06741e914da6d25e49
soakb432 <107282874+soakb432@users.noreply.github.com>
2024-10-15 20:31:23 -0400

    Divide grid into rows

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const container = document.querySelector(".container")
 
-for (let i = 0; i < 16*16; i++) {
-    container.appendChild(document.createElement("div"));
+for (let i = 0; i < 16; i++) {
+    const row = document.createElement("div");
+    row.setAttribute("class", "row");
+    
+    for (let j = 0; j < 16; j++) {
+        const square = document.createElement("div");
+        square.setAttribute("class", "square");
+        row.appendChild(square);
+    }
+
+    container.appendChild(row);
 }
