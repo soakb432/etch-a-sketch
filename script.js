7f50e77123e4c44f54d7198181d324663776db13
soakb432 <107282874+soakb432@users.noreply.github.com>
2024-10-15 20:31:23 -0400

    Change square colors when hovered

--- script.js
+++ script.js
@@ -9,7 +9,11 @@ for (let i = 0; i < 16; i++) {
     
     for (let j = 0; j < 16; j++) {
         const square = document.createElement("div");
-        square.setAttribute("class", "square");
+        square.setAttribute("class", "square"); 
+        // SET MOUSE EVENT HERE
+        square.addEventListener("mouseenter", () => {
+            square.style.backgroundColor = "red";
+        })
         row.appendChild(square);
     }
 
